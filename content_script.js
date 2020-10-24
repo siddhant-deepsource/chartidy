e49142e6e9dbfab36b0d10e1079421c43be7e96e
devcer <santosh.viswanatham@ask.com>
2020-10-24 22:10:47 +0530

    code cleanups, updated chart label dynamically

--- content_script.js
+++ content_script.js
@@ -52,8 +52,7 @@ observer.observe(document.getElementById("historicalData"), {
   attributes: true
 });
 window.addEventListener('load', () => {
-  document.body.innerHTML += modalsample; // Append Modal in the document body
-  const tableParent = document.getElementById('historicalData');
+  document.body.innerHTML += modalsample;
 
   let button = document.createElement('button');
   button.innerText = 'VISUALIZE';
@@ -62,45 +61,49 @@ window.addEventListener('load', () => {
   
   document.getElementById('historicalData').parentNode.insertBefore(button, document.getElementById('historicalData'));
   MicroModal.init({
-    onShow: () => {
-      const table = document.getElementsByTagName('table')[0];
-      headerLabels = getHeaderLabels(table);
-      const xAxisList = document.getElementById('xAxis');
-      const yAxisList = document.getElementById('yAxis');
-
-      headerLabels.forEach((header, index) => {
-        const optionElement = document.createElement('option');
-        optionElement.setAttribute('value', index);
-        optionElement.innerText = header;
-        xAxisList.appendChild(optionElement);
-        yAxisList.appendChild(optionElement.cloneNode(true));
-      });
-
-      let chartContainer = document.createElement('div');
-      chartContainer.classList.add('chart-container');
-      chartContainer.setAttribute('id', 'chart-container');
-      tableParent.appendChild(chartContainer);
-    },
+    onShow
   });
   document.getElementById('form').addEventListener('submit', onSubmitForm);
 });
+
+function onShow() {
+  const table = document.getElementsByTagName('table')[0];
+    tableParent = document.getElementById('historicalData'),
+    headerLabels = getHeaderLabels(table);
+
+    const xAxisList = document.getElementById('xAxis');
+    const yAxisList = document.getElementById('yAxis');
+
+    headerLabels.forEach((header, index) => {
+      const optionElement = document.createElement('option');
+      optionElement.setAttribute('value', index);
+      optionElement.innerText = header;
+      xAxisList.appendChild(optionElement);
+      yAxisList.appendChild(optionElement.cloneNode(true));
+    });
+
+    let chartContainer = document.createElement('div');
+    chartContainer.classList.add('chart-container');
+    chartContainer.setAttribute('id', 'chart-container');
+    tableParent.appendChild(chartContainer);
+}
+
 function onSubmitForm(ev) {
   ev.preventDefault();
   const xIndex = parseInt(ev.target.xAxis.value),
   yIndex = parseInt(ev.target.yAxis.value),
   chartType = ev.target.chartType.value;
-  
+
   const table = document.getElementsByTagName('table')[0];
   const data = getColumnValues(table, xIndex, yIndex);
 
   let chartCanvas = document.createElement('canvas');
   chartCanvas.setAttribute('id', 'chart');
-  chartCanvas.setAttribute('height', 500);
-  chartCanvas.setAttribute('width', 500);
 
   document.getElementById('chart-container').appendChild(chartCanvas);
 
-  const chart = drawChart(chartCanvas, chartType, 'Date vs Closing price', data.xAxisLabels, data.yAxisValues);
+  drawChart(chartCanvas, chartType, `${headerLabels[xIndex]} vs ${headerLabels[yIndex]}`, data.xAxisLabels, data.yAxisValues);
+
   MicroModal.close("input-modal");
 }
 
@@ -116,51 +119,7 @@ function renderVisualizationButtons() {
     // MicroModal.init();
   }
 }
-function addChart() {
-  // document.getElementById("historicalData")
-  let chartContainer = document.createElement('canvas');
-  chartContainer.setAttribute('id', 'chart');
-  chartContainer.setAttribute('height', 500);
-  chartContainer.setAttribute('width', 500);
-  document.getElementById("historicalData").appendChild(chartContainer);
-  var ctx = document.getElementById('chart');
-  var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
-          }
-      }
-  });
-}
+
 /*
 // labels: Array<string> - X axis labels
 // data: Array<number>
@@ -175,17 +134,14 @@ function drawChart(container, chartType, label, xAxislabels, yAxisValues) {
       datasets: [{
           label: label,
           data: yAxisValues,
-          borderWidth: 1
+          borderWidth: 1,
+          fill: false,
+					backgroundColor: chartColors.blue,
+					borderColor: chartColors.blue
       }]
     },
     options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-              beginAtZero: true
-          }
-        }]
-      }
+      responsive: true
     }
   });
   return chart;
